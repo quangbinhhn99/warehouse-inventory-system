d961552f59730892fdd76e4aeda54a613d8a4a78
Quang Bình <quangbinh011199@gmail.com>
2021-05-27 00:04:47 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -12,9 +12,9 @@
 |
 |
 */
-  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_HOST', '139.180.186.119:3346' );          // Set database host
   define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_PASS', 'root' );             // Set database password
   define( 'DB_NAME', 'oswa_inv6' );        // Set database name
 
 ?>
